9d860c6cf2bb46abae06a3fb836a51c991ac58eb
Daniel Rush <rush.daniel95@gmail.com>
2020-03-25 23:22:18 -0700

    lint

--- microplot.py
+++ microplot.py
@@ -18,17 +18,24 @@ module_logger = logging.getLogger('MicroPlot')
 parser = argparse.ArgumentParser(
     prog="MicroPlot",
     description="Microplot is a module as well as an executable, \
-                 running it as an executable allows you to use the code generation functions"
+                 running it as an executable allows you to use the \
+                 code generation functions"
 )
-parser.add_argument("-g", "--generate", help="Run code generation tools", action="store_true", default=False)
+parser.add_argument("-g",
+                    "--generate",
+                    help="Run code generation tools",
+                    action="store_true",
+                    default=False)
 parser.add_argument("--force_c",
                     help="If you prefer to use pure C for your embedded code, \
                           use this option to force generation of C code",
                     default=False,
                     action="store_true")
 parser.add_argument("--first_generation",
-                    help="This flag generates the one-time files which will be maintained by user \
-                          after first generation these files are: \'microplot_init.h/c/cc\' and \
+                    help="This flag generates the one-time files which will \
+                          be maintained by user \
+                          after first generation these files are: \
+                          \'microplot_init.h/c/cc\' and \
                           \'microplot_actions.h/cc/c\'",
                     default=False,
                     action="store_true")
@@ -36,6 +43,7 @@ parser.add_argument("--proto_yaml",
                     help="Protocol description yaml file.",
                     default="example_protocol.yml")
 ARGS = parser.parse_args()
+
 
 class MicroPlot():
     def __init__(self, protocol_yaml):
@@ -46,21 +54,24 @@ class MicroPlot():
         if os.path.isfile(protocol_yaml):
             with open(protocol_yaml) as file_pointer:
                 self._yaml_data = yaml.full_load(file_pointer)
-                self._logger.info("Loaded %s as protocol description" % (protocol_yaml))
+                self._logger.info("Loaded %s as protocol description" %
+                                  (protocol_yaml))
         else:
-            logger.fatal("Protocol file %s doesn't exist in the file system" % (protocol_yaml))
+            logger.fatal("Protocol file %s doesn't exist in the file system" %
+                         (protocol_yaml))
             sys.exit(1)
         self._pattern_list = list()
-        for element_key, element_value in self._yaml_data['Protocol']['Pattern'].items():
+        pattern = self._yaml_data['Protocol']['Pattern'].items()
+        for element_key, element_value in pattern:
             self._pattern_list.insert(int(element_key), element_value)
         self._logger.info("Protocol file parsed successfully")
 
     def parse(self, input_string):
         '''
-            Iterate through the pattern list, make sure that each element is the first in the
-            string then pop off of string, if we are looking at the command in the list we expect
-            the delimiter to immediately follow, then if we are looking at the value, pull the
-            integer value out of it
+            Iterate through the pattern list, make sure that each element is
+            the first in the string then pop off of string, if we are looking
+            at the command in the list we expect the delimiter to immediately
+            follow, then if we are looking at the value, pull the integer out
         '''
         working_string = input_string
         value = None
@@ -68,52 +79,67 @@ class MicroPlot():
         for element in self._pattern_list:
             if element == "Value":
                 # TODO(Daniel): handle values other than ints
-                value = int(re.search(r'[-+]?\d*\.?\d+([eE][-+]?\d+)?', working_string).group())
+                value = int(re.search(r'[-+]?\d*\.?\d+([eE][-+]?\d+)?',
+                            working_string).group())
                 working_string = working_string.split(str(value))[-1]
             elif element == "Command":
-                # TODO(Daniel): handle command formats other then command then delimiter
-                split_string = working_string.split(self._yaml_data["Protocol"]["Delimiter"])
+                # TODO(Daniel): handle command formats
+                split_string = working_string.split(
+                    self._yaml_data["Protocol"]["Delimiter"])
                 key = split_string[0]
                 working_string = split_string[1]
             else:
-                split_string = working_string.split(self._yaml_data["Protocol"][element])
+                split_string = working_string.split(
+                    self._yaml_data["Protocol"][element])
                 if len(split_string) > 1:
                     if split_string[0] == '':
                         working_string = split_string[1]
             if key is not None and value is not None:
-                for command_key, command_value in self._yaml_data["Protocol"]["Commands"].items():
+                commands = self._yaml_data["Protocol"]["Commands"].items()
+                for command_key, command_value in commands:
                     if key == command_value:
-                        self._logger.info("Parsed %s with value %d" % (command_key, value))
-                        return {command_key : value}
+                        self._logger.info("Parsed %s with value %d" %
+                                          (command_key, value))
+                        return {command_key: value}
         return None
+
     def generate(self, force_c, generate_actions):
         self._logger.info("Starting Generator")
-        templates = [Template(filename="mako_files/microplot.h.mako")]
+        templates = [Template(
+            filename="mako_files/microplot.h.mako")]
         if not force_c:
-            templates.append(Template(filename="mako_files/microplot_parse.cc.mako"))
+            templates.append(Template(
+                filename="mako_files/microplot_parse.cc.mako"))
         else:
-            templates.append(Template(filename="mako_files/microplot_parse.c.mako"))
+            templates.append(
+                Template(filename="mako_files/microplot_parse.c.mako"))
         if generate_actions:
             if not force_c:
-                templates.append(Template(filename="mako_files/microplot_init.cc.mako"))
-                templates.append(Template(filename="mako_files/microplot_actions.cc.mako"))
+                templates.append(
+                    Template(filename="mako_files/microplot_init.cc.mako"))
+                templates.append(
+                    Template(filename="mako_files/microplot_actions.cc.mako"))
             else:
-                templates.append(Template(filename="mako_files/microplot_init.c.mako"))
-                templates.append(Template(filename="mako_files/microplot_actions.c.mako"))
+                templates.append(
+                    Template(filename="mako_files/microplot_init.c.mako"))
+                templates.append(
+                    Template(filename="mako_files/microplot_actions.c.mako"))
         for template in templates:
-            output_filename = (template.filename.split('/')[-1]).split(".mako")[0]
+            output_filename = template.filename.split('/')[-1]
+            output_filename = output_filename.split(".mako")[0]
             output_filename = "generated_" + output_filename
             output_file = open(output_filename, "w+")
             output_file.write(template.render())
-            self._logger.info("Genererated %s as %s" % (template.filename, output_filename))
+            self._logger.info("Genererated %s as %s" %
+                              (template.filename, output_filename))
             output_file.close()
-
 
 
 def main():
     mp = MicroPlot(ARGS.proto_yaml)
     if ARGS.generate:
         mp.generate(ARGS.force_c, ARGS.first_generation)
+
 
 if __name__ == "__main__":
     main()
--- microplot_test.py
+++ microplot_test.py
@@ -2,11 +2,15 @@ import microplot
 import unittest
 import yaml
 
+
 class TestMicroPlot(unittest.TestCase):
+
     def test_instantiation(self):
-        mp = microplot.MicroPlot("example_protocol.yml")
+        unused_mp = microplot.MicroPlot("example_protocol.yml")
+        print(unused_mp)
         try:
-            mp2 = microplot.MicroPlot("")
+            unused_mp2 = microplot.MicroPlot("")
+            print(unused_mp2)
         except SystemExit as e:
             print("Caught %s as expected" % (e))
 
@@ -20,18 +24,17 @@ class TestMicroPlot(unittest.TestCase):
             self.assertNotEqual(None, response)
             self.assertEqual(command, list(response.keys())[0])
             if "Analog" in list(response.keys())[0]:
-              self.assertEqual(response[command], 1000 + iteration)
+                self.assertEqual(response[command], 1000 + iteration)
             elif "Digital" in response.keys():
-              self.assertEqual(response[command], (-100 - iteration + 9))
+                self.assertEqual(response[command], (-100 - iteration + 9))
             iteration += 1
+
     def test_generate(self):
         mp = microplot.MicroPlot("example_protocol.yml")
         mp.generate(False, False)
         mp.generate(False, True)
         mp.generate(True, False)
         mp.generate(True, True)
-
-
 
 
 if __name__ == "__main__":
